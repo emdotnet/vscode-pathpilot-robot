2044df4918ec4312976284ed110f3879484a07b9
Jakub Fišer <jakub.fiser@eryaf.com>
2023-02-12 20:12:08 +0100

    MAINTENANCE: Change logic of importing Dockerode
    Make the Dockerode import Typescript-like. Do not use the require function when everything can be imported from the TypeScript typing packages.

--- src/containers.ts
+++ src/containers.ts
@@ -1,15 +1,11 @@
-import Dockerode from 'dockerode';
+import Docker, {ContainerInfo} from 'dockerode';
 
-const dockerode = require('dockerode'); /* TODO: This is ugly!
-                                           Try to solve the issue so than only
-                                           one declaration or import suffice */
-
-const docker = new dockerode();
+const docker = new Docker();
 
 export async function checkForRunningContainer(containerName: string): Promise<boolean> {
-    let runningContainers: Array<Dockerode.ContainerInfo> = await docker.listContainers();
+    let runningContainers: Array<ContainerInfo> = await docker.listContainers();
 
-    return runningContainers.some((container: Dockerode.ContainerInfo) => {
+    return runningContainers.some((container: ContainerInfo) => {
         if (container.Names.includes(`/${containerName}`)) {
             if (container.State === "running") {
                 return true;
